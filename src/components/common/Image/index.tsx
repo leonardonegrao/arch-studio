c7d0fde5defd04b280ac3198888026fd4f132726
Leonardo Negrão <leonardolnegrao@gmail.com>
2021-09-11 17:10:02 -0300

    fix(image): add types declarations on Image to match Next's

--- src/components/common/Image/index.tsx
+++ src/components/common/Image/index.tsx
@@ -2,7 +2,18 @@ import NextImage, { ImageProps as NextImageProps } from 'next/image';
 
 import { ImageWrapper } from './style';
 
-declare type ImageProps = Omit<NextImageProps, 'width' | 'height'> & {
+interface StaticImageData {
+  src: string;
+  height: number;
+  width: number;
+  blurDataURL?: string;
+}
+interface StaticRequire {
+  default: StaticImageData;
+}
+declare type StaticImport = StaticRequire | StaticImageData;
+
+declare type ImageProps = Omit<NextImageProps, 'width' | 'height' | 'blurDataURL'> & {
   darkenFilter?: boolean;
   responsiveWidth: {
     sm: string;
@@ -16,12 +27,11 @@ declare type ImageProps = Omit<NextImageProps, 'width' | 'height'> & {
   };
 }
 
-export default function Image({ responsiveHeight, responsiveWidth, darkenFilter = false, ...rest }: ImageProps) {
-  console.log(rest.alt, darkenFilter);
-
+export default function Image({ responsiveHeight, responsiveWidth, darkenFilter = false, src, ...rest }: ImageProps) {
   return (
     <ImageWrapper width={responsiveWidth} height={responsiveHeight}>
       <NextImage
+        src={src as StaticImport}
         {...rest}
         objectFit="cover"
         layout="fill"
